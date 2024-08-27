74590aa2fc038d85361a52f89e8e7e6b4c82ce6a
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2024-08-27 08:56:10 +0700

    Add files via upload

---
+++ Number 2.java
@@ -0,0 +1,14 @@
+public class Number{
+    public int i;
+}
+public class Assignment2{
+    public static void main(String[] args){
+        Number N1 = new Number();
+        Number N2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1 = n2;
+        n2.i =10;
+        n1.i = 20;
+    }
+}
