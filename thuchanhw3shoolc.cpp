de6bba9861ff1b876d0acc3c9a5b960c2c8b3446
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2024-08-29 07:19:19 +0700

    Add files via upload

---
+++ thuchanhw3shoolc.cpp
@@ -0,0 +1,14 @@
+public
+class Main
+{
+public
+    static void main(String[] args)
+    {
+        int x = 80;
+        int y = 50;
+        if (x < y)
+        {
+            System.out.println("x is greater than y");
+        }
+    }
+}
