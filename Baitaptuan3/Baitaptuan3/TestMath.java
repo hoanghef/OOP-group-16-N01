dbd26800caeb69d95af3f07b84a5f4e54ae3d6ac
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2024-09-17 00:41:25 +0000

    TestMath

---
+++ Baitaptuan3/Baitaptuan3/TestMath.java
@@ -0,0 +1,10 @@
+public class Arithmetic{
+    public static void main(String[] args){
+        int a = 231;
+        int b = 49;
+        System.out.println(  (a+b));
+        System.out.println(  (a-b));
+        System.out.println( (a*b));
+        System.out.println( ((float)a/b));
+    }
+}
